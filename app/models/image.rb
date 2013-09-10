58543d4332e151b26c449eef3f519a3f2cc94303
Liz Abinante <eabinante@gmail.com>
2013-09-10 16:18:14 -0500

    update models, migrations to handle paperclip images; add image fields to new pattern form

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,7 +1,8 @@
 class Image < ActiveRecord::Base
-  attr_accessible :url, :pattern
+  attr_accessible :photo, :pattern
 
   belongs_to :pattern
+  has_attached_file :photo, :styles => { :large => "540x540", :medium => "300x300>", :thumb => "100x100>" }
 
-  validates_presence_of :url, :pattern
+  validates_presence_of :photo, :pattern
 end
