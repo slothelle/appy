84c8033094a12feed2b07dfc51b194c916ba5def
Liz Abinante <eabinante@gmail.com>
2013-09-11 10:58:29 -0500

    create before_filter that requires login to access entire site

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   include ApplicationHelper
+
+  before_filter :require_login
+
+  private
+  def require_login
+    unless current_user
+      redirect_to log_in_path
+    end
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+  skip_before_filter :require_login
+
   def new
     @user = User.new
   end
@@ -7,7 +9,7 @@ class SessionsController < ApplicationController
     @user = User.find_by_email(params[:session][:email])
     if @user && @user.authenticate(params[:session][:password])
       create_session
-      redirect_to user_path(@user.id)
+      redirect_to patterns_path
     elsif @user.nil?
       @errors = "An account with the email address #{params[:session][:email]} not found."
       render "new"
