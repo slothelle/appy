387b0be7c4a01435ac33b21c9a0f1ddf00661416
Liz Abinante <eabinante@gmail.com>
2013-09-14 09:57:59 -0500

    add legends#new view/action; legends#create wip; legends#destroy done

---
+++ app/controllers/chart_legends_controller.rb
@@ -0,0 +1,26 @@
+class ChartLegendsController < ApplicationController
+  def new
+    @legend = ChartLegend.new
+    @pattern = Pattern.find(params[:pattern_id])
+    @legends = @pattern.chart_legends
+  end
+
+  def create
+    legend = ChartLegend.new(params[:legend])
+    legend.pattern = Pattern.find(params[:pattern_id])
+    if legend.save
+      flash[:notice] = "Legend #{legend.image_file_name} successfully added to #{chart.pattern.name}"
+      redirect_to patterns_path
+    else
+      redirect_to new_pattern_chart_legend_path(params[:pattern_id])
+    end
+  end
+
+  def destroy
+    ChartLegend.destroy(params[:id])
+  end
+
+  def destroy
+    # more magic
+  end
+end
